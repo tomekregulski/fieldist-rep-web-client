2186e823cf7ed265ffef283dc9326549ea459a7b
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-12-12 13:11:55 -0500

    Convert expense table to form. Add photo preview and clear expenses

--- src/components/ExpensePhotos/ExpensePhotos.js
+++ src/components/ExpensePhotos/ExpensePhotos.js
@@ -1,11 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { ReportContext } from '../../context/ReportContext';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useEffect, useState } from 'react';
 
 import Photos from '../Photos/Photos';
+import YesNoAlert from '../YesNoAlert/YesNoAlert';
 
 const ExpensePhotos = (props) => {
-  // const { expPhotos } = useContext(ReportContext);
   const [expensePhoto, setExpensePhoto] = useState('');
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     props.callback({ expensePhoto: expensePhoto });
@@ -15,9 +16,41 @@ const ExpensePhotos = (props) => {
     setExpensePhoto(data);
   };
 
+  const closeModal = () => {
+    setShow(false);
+  };
+
+  const deletePhoto = () => {
+    setExpensePhoto('');
+    setShow(false);
+  };
+
   return (
     <div>
+      {show && (
+        <YesNoAlert
+          open={show}
+          message='Would you like to delete this photo?'
+          handleClose={closeModal}
+          callback={deletePhoto}
+        />
+      )}
       <Photos callback={handleExpensePhotos} type='expense_photo' />
+      {expensePhoto && (
+        <div style={{}}>
+          <img
+            onClick={() => setShow(true)}
+            style={{
+              display: 'block',
+              width: '60%',
+              maxWidth: '100px',
+              margin: '25px auto 0',
+            }}
+            src={expensePhoto}
+            alt={'photo of the receipt'}
+          />
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/PhotoDeleteButton/PhotoDeleteButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PhotoDeleteButton.css';
+
+const PhotoDeleteButton = () => {
+  return <button>X</button>;
+};
+
+export default PhotoDeleteButton;
